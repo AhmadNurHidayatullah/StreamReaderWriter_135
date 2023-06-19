fe8e0c1628d397f6be8cd1ee879865bd10b5b077
AhmadNurHidayatullah <127074146+AhmadNurHidayatullah@users.noreply.github.com>
2023-06-19 10:59:39 +0700

    menambahkan fungsi while dalam int main

--- iofilewithpath/iofilewithpath.cpp
+++ iofilewithpath/iofilewithpath.cpp
@@ -17,6 +17,18 @@ int main()
     //menunjuk ke sebuah nama file
     outfile.open(namafile, ios::out);
 
+    cout << ">= menulis file,\'q\' untuk keluar " << endl;
+
+    //unlimited loop untuk menulis
+    while (true) {
+        cout << "_ ";
+        //mendapatkan setiap karakter dalam satu baris
+        getline(cin, baris);
+        //loop akan berhenti jika anda memasukan karakter q
+        if (baris == "q") break;
+        //menulis dan memasukan nilai dari 'baris' ke dalam file
+        outfile << baris << endl;
+    }
 }
 
 
