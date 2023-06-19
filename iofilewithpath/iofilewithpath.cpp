4e7f09bf8e5bf5b59e391b6ca440079ab61720f0
AhmadNurHidayatullah <127074146+AhmadNurHidayatullah@users.noreply.github.com>
2023-06-19 10:54:35 +0700

    menambahkan fungsi ofstream dan outfile dalam int main

--- iofilewithpath/iofilewithpath.cpp
+++ iofilewithpath/iofilewithpath.cpp
@@ -7,6 +7,16 @@ using namespace std;
 int main()
 {
     string baris;
+    string namafile;
+
+    cout << "masukkan nama file : ";
+    cin >> namafile;
+
+    //membuka file dalam mode menulis.
+    ofstream outfile;
+    //menunjuk ke sebuah nama file
+    outfile.open(namafile, ios::out);
+
 }
 
 
