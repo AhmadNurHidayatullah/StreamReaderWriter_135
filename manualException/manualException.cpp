f764c866f7d50ac113404f613fb5faf48ac3f1e2
AhmadNurHidayatullah <127074146+AhmadNurHidayatullah@users.noreply.github.com>
2023-06-19 11:16:59 +0700

    menambahkan fungsi try,catch(int a), dan catch (...)

--- manualException/manualException.cpp
+++ manualException/manualException.cpp
@@ -3,9 +3,22 @@ using namespace std;
 
 int main()
 {
-    try {
-
+     try{
+         cout << "selamat belajar di prodi TI UMY" << endl;
+         //throw 0.5; //melemparkan sebuah integer maka
+         cout << "peryataan tidak akan dieksekusi" << endl;
+     }
+     catch (int a) {
+         //blok ini akan dieksekusi
+         cout << "pengecualian akan dieksekusi" << endl;
+     }
+     catch (...) {
+         /*jika selain integer maka block ini akan dieksekusi*/
+         cout << "default pengecualian di eksekusi" << endl;
     }
+
+     return 0;
 }
+
 
 
